e1cdcdc0ceb0b00d3176ee8fa531aea05b1c52e1
Yogaledare <marcus_uppstrom@hotmail.com>
2024-04-04 14:14:07 +0200

    cleaned up unused parts

--- bishbosh.js
+++ bishbosh.js
@@ -48,20 +48,12 @@ document.addEventListener('DOMContentLoaded', () => {
             const bosh = document.getElementById('boshInput').value;
             const limit = document.getElementById('limitInput').value;
             
-            console.log(` hello ${bish}, ${bosh}, ${limit}`)
-            
             const results = BishBoshImperative(bish, bosh, limit); 
-            
             const resultDiv = document.getElementById('result');
             resultDiv.innerHTML = ''; 
             
-            const row = document.createElement('div');
-            row.className = 'row row-cols-md-6 g-4';
-
             let cardsHtml = results.map((result, index) => MakeBisBoshCard(result, index))
-
-            document.getElementById('result').innerHTML =
-                `<div class="row g-4">${cardsHtml.join('')}</div>`;
+            resultDiv.innerHTML = `<div class="row g-4">${cardsHtml.join('')}</div>`;
         })
 });
 
@@ -76,88 +68,4 @@ const BishBoshDeclarative = (bish = 3, bosh = 4, limit = 100) => {
 let output = BishBoshImperative(); 
 output.map(v => console.log(v))
 
-
-
-// results.forEach((result, index) => {
-//     const col = document.createElement('div');
-//     col.className = 'col';
-//
-//     const card = document.createElement('div');
-//     card.className = 'card compact-card'; 
-//
-//     const cardBody = document.createElement('div');
-//     cardBody.className = 'card-body';
-//
-//     const cardTitle = document.createElement('h6'); 
-//     cardTitle.className = 'card-title';
-//     cardTitle.textContent = `#${index + 1}`;
-//
-//     const cardText = document.createElement('p');
-//     cardText.className = 'card-text';
-//     cardText.textContent = result;
-//
-//     cardBody.appendChild(cardTitle);
-//     cardBody.appendChild(cardText);
-//     card.appendChild(cardBody);
-//     col.appendChild(card);
-//     row.appendChild(col);
-// });
-
-// resultDiv.appendChild(row);
-
-
-
-//
-// results.forEach((result, index) => {
-//     const col = document.createElement('div');
-//     col.className = 'col';
-//
-//     const card = document.createElement('div');
-//     card.className = 'card h-100';
-//
-//     const cardBody = document.createElement('div');
-//     cardBody.className = 'card-body';
-//
-//     const cardTitle = document.createElement('h5');
-//     cardTitle.className = 'card-title';
-//     cardTitle.textContent = `#${index + 1}`;
-//
-//     const cardText = document.createElement('p');
-//     cardText.className = 'card-text';
-//     cardText.textContent = result;
-//
-//     cardBody.appendChild(cardTitle);
-//     cardBody.appendChild(cardText);
-//     card.appendChild(cardBody);
-//     col.appendChild(card);
-//     row.appendChild(col);
-// });
-//
-
-// BishBosh();
-
-// let declarative = BishBoshDeclarative(); 
-// declarative.map(i => console.log(i))
-
-
-
-// const BishBosh = (bish = 3, bosh = 4, limit = 100) => {
-//     for (let i = 1; i <= limit; i++) {
-//         let output = GetBishBosh(i, bish, bosh);
-//         console.log(output); 
-//     }
-// }
-
-
-
-
-// if (i % bish === 0 && i % bosh === 0) {
-//     console.log('Bish-Bosh')
-// } else if (i % bish === 0) {
-//     console.log('Bish');
-// } else if (i % bosh === 0) {
-//     console.log('Bosh');
-// } else {
-//     console.log(i);
-// }
 
